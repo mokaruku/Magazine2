e774bca9c78b081b17af9c0243983952d84d8917
mokaruku <web.it.reico29@gmail.com>
2024-07-26 18:00:31 +0900

    メインアニメーション実装完了

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ const navToggle = document.querySelector('.header__nav-toggle');
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('open');
 })
+
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
+})
